3bc3bf2ece73f6ad9b6ca8ac9e6f65a92d411988
Chineek Favors <chineekfavors01@gmail.com>
2019-06-26 21:12:04 -0700

    added data attribute to corresponding html elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('alive');
+class TabLink {
+	constructor(link){
+		this.link = link;
+		this.dataAttr = this.link.dataset.tab;
+	}
+}
+// grabing dom elements with class name of link
+const links = document.querySelectorAll('.link');
+// iterate over individual links 
+links.forEach(tab => new TabLink(tab));
+
