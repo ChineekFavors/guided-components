3606a3a7d38aedad77fd1a82e5b14cb9f87c954d
Chineek Favors <chineekfavors01@gmail.com>
2019-06-27 00:38:59 -0700

    reference content by dataset.tab

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 class TabLink {
-	constructor(link){
-		this.link = link;
-		this.dataAttr = this.link.dataset.tab;
+	constructor(tabElement){
+		//this.tabElementProperty = tabElement to have reference to it later
+		this.tabElementProperty = tabElement;
+
+		//reference to data attribute number using dataset.tab at end
+		this.tabNumber = this.tabElementProperty.dataset.tab;
+
+		//reference to content specific to its dataset tab value
+		this.content = document.querySelector(`div.content[data-tab="${this.tabNumber}"]`);
+		
+		//eventlistener on this.tabElementProperty element 
+		//and toggle class name from this.content
+		this.tabElementProperty.addEventListener('click', () => {
+			this.content.classList.toggle('change');
+		})
+
 	}
+
 }
 // grabing dom elements with class name of link
-const links = document.querySelectorAll('.link');
+const tabs = document.querySelectorAll('.link');
 // iterate over individual links 
-links.forEach(tab => new TabLink(tab));
+tabs.forEach(tab => new TabLink(tab));
 
